69c2ea50a95af86911b78045a4aeed79d0253b67
aa1aac <35446762+aa1aac@users.noreply.github.com>
2020-10-02 12:30:28 +0545

    add validation check to the signup route

--- server/routes/AuthRoute.ts
+++ server/routes/AuthRoute.ts
@@ -1,4 +1,7 @@
-import express, { Router } from "express";
+import express, { Router, Request, Response, NextFunction } from "express";
+import { check, validationResult } from "express-validator";
+
+import { UserSignup } from "../controller/AuthController";
 
 const router: Router = express.Router();
 
@@ -12,7 +15,49 @@ router.get("/logout");
 
 // /api/auth/signup
 // POST
-router.post("/signup");
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("the name should have minimum length of 3"),
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("the name must have minimum length of 3")
+      .trim(),
+
+    check("email")
+      .isEmail()
+      .withMessage("invalid email address")
+      .normalizeEmail(),
+
+    check("password")
+      .isLength({ min: 8, max: 15 })
+      .withMessage("your password should have min and max length between 8-15")
+      .matches(/\d/)
+      .withMessage("your password should have at least one number")
+      .matches(/[!@#$%^&*(),.?":{}|<>]/)
+      .withMessage("your password should have at least one sepcial character"),
+
+    check("confirmPassword").custom((value: string, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("confirm password does not match");
+      }
+      return true;
+    }),
+  ],
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).formatWith(
+      ({ msg }: { msg: string }) => msg
+    );
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    return next();
+  },
+  UserSignup
+);
 
 // /api/auth/get-user
 // GET
--- server/server.ts
+++ server/server.ts
@@ -14,7 +14,7 @@ app.use("/api/auth", AuthRoute);
 
 if (process.env.MONGO_URI) {
   mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("connected to mongo db database"))
     .catch((e: Error) => console.error(e));
 }
